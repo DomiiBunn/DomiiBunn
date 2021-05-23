082a13f73600c78de65c2369ad2179e547b3ae30
DomiiBunni <domi.jadowska@gmail.com>
2021-05-23 16:41:29 +0200

    Add a navBar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,40 @@
 <template>
   <div id="app">
+    <navigation />
     <router-view />
   </div>
 </template>
 
-<style lang="scss"></style>
+<script>
+import navigation from "@/components/navBar.vue";
+export default {
+  components: {
+    navigation,
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Great+Vibes&family=Roboto&display=swap");
+html {
+  height: 100%;
+  background-color: white;
+}
+
+body {
+  margin: 0;
+  height: 100%;
+  background-image: url("http://i.imgur.com/9HMnxKs.png");
+  background-repeat: repeat-y;
+  background-size: 40% auto;
+  font-family: "Roboto", sans-serif;
+}
+
+a,
+a:hover,
+a:focus,
+a:active {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
---
+++ src/components/navBar.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav>
+    <div class="title">
+      <div class="name">Dominika Jadowska</div>
+      <div class="slash">/</div>
+      <div class="position">Service desk analyst</div>
+    </div>
+    <div class="links">
+      <router-link to="/" class="link">Home</router-link>
+      <router-link to="/resume" class="link">Resume</router-link>
+      <router-link to="/career" class="link">Career</router-link>
+      <router-link to="/projects" class="link">Projects</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  height: 65px;
+  background-color: white;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.4);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 2rem;
+  .title {
+    display: flex;
+    gap: 5px;
+    height: 100%;
+    align-items: center;
+    .name {
+      font-family: "Great Vibes", cursive;
+      font-size: 2rem;
+      color: rgb(88, 15, 88);
+      text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.4);
+    }
+    .slash {
+      color: blue;
+    }
+    .position {
+      font-size: 0.8rem;
+    }
+  }
+  .links {
+    display: flex;
+    gap: 10px;
+    .link {
+      text-shadow: none;
+      transition: text-shadow 150ms linear;
+    }
+    .link:hover {
+      text-shadow: 1px 1px 1px rgba(88, 15, 88, 0.4);
+    }
+    .link.router-link-exact-active {
+      color: rgb(88, 15, 88);
+      font-weight: bold;
+    }
+  }
+}
+</style>
